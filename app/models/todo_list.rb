55119c1362836cb0e7a5680b22174791ee19a2e1
finn <fterdal@gmail.com>
2015-03-23 03:06:04 -0400

    Added testing for the create to_do list feature

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,5 @@
 class TodoList < ActiveRecord::Base
+	validates :name, presence: true
+	validates :name, length: { minimum: 3 }
 end
+
---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "Creating TODO lists" do
+	it "redirects to the TODO index page on success" do
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+		
+		fill_in "Name", with: "My todo list"
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My todo list")
+
+	end
+
+	it "displays an error when the TODO list has no name" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+		
+		fill_in "Name", with: ""
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
+
+	it "displays an error when the name isn't at least 3 characters long" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+		
+		fill_in "Name", with: "Hi"
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
+	
+end
