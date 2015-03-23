d606d61b89f94f9626eb75caf35faa0929a8e94b
finn <fterdal@gmail.com>
2015-03-23 03:19:33 -0400

    Added testing to accomodates description presence and length (minimum 5)

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,8 @@
 class TodoList < ActiveRecord::Base
 	validates :name, presence: true
 	validates :name, length: { minimum: 3 }
+
+	validates :description, presence: true
+	validates :description, length: {  minimum: 5 }
 end
 
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -47,5 +47,39 @@ describe "Creating TODO lists" do
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
+
+	it "displays an error when the TODO list has no description" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+		
+		fill_in "Name", with: "Best list"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
+	
+	it "displays an error when the name isn't at least 3 characters long" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+		
+		fill_in "Name", with: "Best list"
+		fill_in "Description", with: "Hi"
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
 	
 end
