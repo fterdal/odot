0b98cd44067e6f244d4b979a631e5d6a4e61905e
finn <fterdal@gmail.com>
2015-03-23 03:32:27 -0400

    Refactored the create_list test

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,83 +1,56 @@
 require 'spec_helper'
 
 describe "Creating TODO lists" do
-	it "redirects to the TODO index page on success" do
+	def create_todo_list(options={})
+		options[:name] ||="My todo list"
+		options[:description] ||= "This is what I'm doing today."
+
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 		
-		fill_in "Name", with: "My todo list"
-		fill_in "Description", with: "This is what I'm doing today."
+		fill_in "Name", with: options[:name]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
-
+	end
+	
+	it "redirects to the TODO index page on success" do
+		create_todo_list
 		expect(page).to have_content("My todo list")
-
 	end
 
 	it "displays an error when the TODO list has no name" do
 		expect(TodoList.count).to eq(0)
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-		
-		fill_in "Name", with: ""
-		fill_in "Description", with: "This is what I'm doing today."
-		click_button "Create Todo list"
-
+		create_todo_list name: ""
 		expect(TodoList.count).to eq(0)
 		expect(page).to have_content("error")
-		
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
 
 	it "displays an error when the name isn't at least 3 characters long" do
 		expect(TodoList.count).to eq(0)
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-		
-		fill_in "Name", with: "Hi"
-		fill_in "Description", with: "This is what I'm doing today."
-		click_button "Create Todo list"
-
+		create_todo_list name: "Hi"
 		expect(TodoList.count).to eq(0)
 		expect(page).to have_content("error")
-		
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
 
 	it "displays an error when the TODO list has no description" do
 		expect(TodoList.count).to eq(0)
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-		
-		fill_in "Name", with: "Best list"
-		fill_in "Description", with: ""
-		click_button "Create Todo list"
-
+		create_todo_list description: ""
 		expect(TodoList.count).to eq(0)
 		expect(page).to have_content("error")
-		
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
 	
-	it "displays an error when the name isn't at least 3 characters long" do
+	it "displays an error when the name isn't at least 5 characters long" do
 		expect(TodoList.count).to eq(0)
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
-		
-		fill_in "Name", with: "Best list"
-		fill_in "Description", with: "Hi"
-		click_button "Create Todo list"
-
+		create_todo_list description: "Mine"
 		expect(TodoList.count).to eq(0)
 		expect(page).to have_content("error")
-		
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
